b7ca346f366029f02cbe9d6361ad886d7490f929
Lisa Lemons <me@lisa-lemons.com>
2020-12-29 14:30:23 -0500

    Added cookie persitance

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
-import express, {Application} from 'express';
-import {ApolloServer} from 'apollo-server-express';
-import {connectDatabase} from './database';
+import express, { Application } from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { connectDatabase } from './database';
+import cookieParser from 'cookie-parser';
 import { typeDefs, resolvers } from './graphql';
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
+
+  app.use(cookieParser(process.env.SECRET));
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => ({ db }),
+    context: ({ req, res }) => ({ db, req, res }),
   });
-  server.applyMiddleware({app, path: '/api'});
-
+  server.applyMiddleware({ app, path: '/api' });
 
   app.listen(process.env.PORT);
 
