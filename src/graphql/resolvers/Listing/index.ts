766e1923df8eb745b32dccc78feebeabe1dee022
Lisa Lemons <me@lisa-lemons.com>
2021-01-08 12:16:49 -0500

    Listing resolvers updated

--- src/graphql/resolvers/Listing/index.ts
+++ src/graphql/resolvers/Listing/index.ts
@@ -1,10 +1,86 @@
 import { IResolvers } from 'apollo-server-express';
-import { Listing } from '../../../lib/types';
+import { Request } from 'express';
+import { ObjectID } from 'mongodb';
+import { Database, Listing, User } from '../../../lib/types';
+import { authorize } from '../../../lib/utils';
+import { ListingArgs, ListingBookingsArgs, ListingBookingsData } from './types';
 
 export const listingResolvers: IResolvers = {
+  Query: {
+    listing: async (
+      _root: undefined,
+      { id }: ListingArgs,
+      { db, req }: { db: Database; req: Request }
+    ): Promise<Listing> => {
+      try {
+        const listing = await db.listings.findOne({ _id: new ObjectID(id) });
+
+        if (!listing) {
+          throw new Error('Listing not found.');
+        }
+
+        const viewer = await authorize(db, req);
+
+        if (viewer && viewer._id === listing.host) {
+          listing.authorized = true;
+        }
+
+        return listing;
+      } catch (error) {
+        throw new Error(`Failed to query listing: ${error}`);
+      }
+    },
+  },
   Listing: {
     id: (listing: Listing): string => {
       return listing._id.toString();
+    },
+    host: async (
+      listing: Listing,
+      // eslint-disable-next-line @typescript-eslint/ban-types
+      _args: {},
+      { db }: { db: Database }
+    ): Promise<User> => {
+      const host = await db.users.findOne({ _id: listing.host });
+
+      if (!host) {
+        throw new Error('Host not be found.');
+      }
+
+      return host;
+    },
+    bookingsIndex: (listing: Listing): string => {
+      return JSON.stringify(listing.bookingsIndex);
+    },
+    bookings: async (
+      listing: Listing,
+      { limit, page }: ListingBookingsArgs,
+      { db }: { db: Database }
+    ): Promise<ListingBookingsData | null> => {
+      try {
+        if (!listing.authorized) {
+          return null;
+        }
+
+        const data: ListingBookingsData = {
+          total: 0,
+          result: [],
+        };
+
+        let cursor = await db.bookings.find({
+          _id: { $in: listing.bookings },
+        });
+
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0);
+        cursor = cursor.limit(limit);
+
+        data.total = await cursor.count();
+        data.result = await cursor.toArray();
+
+        return data;
+      } catch (error) {
+        throw `Failed to query listing's bookings: ${error}`;
+      }
     },
   },
 };
