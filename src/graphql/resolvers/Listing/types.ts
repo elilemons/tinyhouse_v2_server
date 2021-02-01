87b09bfc0927ccb8c5352a807f95668c255928f1
Lisa Lemons <me@lisa-lemons.com>
2021-02-01 13:49:30 -0500

    Resolvers and types for the home page listings in UI

--- src/graphql/resolvers/Listing/types.ts
+++ src/graphql/resolvers/Listing/types.ts
@@ -1,7 +1,18 @@
-import { Booking } from '../../../lib/types';
+import { Booking, Listing } from '../../../lib/types';
+
+export enum ListingsFilter {
+  PRICE_LOW_TO_HIGH = 'PRICE_LOW_TO_HIGH',
+  PRICE_HIGH_TO_LOW = 'PRICE_HIGH_TO_LOW',
+}
 
 export interface ListingArgs {
   id: string;
+}
+
+export interface ListingsArgs {
+  filter: ListingsFilter;
+  limit: number;
+  page: number;
 }
 
 export interface ListingBookingsArgs {
@@ -12,4 +23,9 @@ export interface ListingBookingsArgs {
 export interface ListingBookingsData {
   total: number;
   result: Booking[];
+}
+
+export interface ListingsData {
+  total: number;
+  result: Listing[];
 }
